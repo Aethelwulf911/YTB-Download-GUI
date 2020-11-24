5f395386f0cd39890af8f70bf54f9a515964a3fc
BOUSSOURA Mohamed Cherif <65354249+Aethelwulf911@users.noreply.github.com>
2020-11-24 18:11:57 +0100

    Update youtube-download.py

--- youtube-download.py
+++ youtube-download.py
@@ -1,30 +1,26 @@
-import tkinter as tk
-from pytube import YouTube
-def downloadVid():
-    global E1
-    string =E1.get()
-    yt = YouTube(str(string))
-    videos = yt.get_videos()
-    s=1
-    for v in videos:
-        print(str(s) + '.' + str(v))
-        s +=1
-    n=int(input("Enter your choice"))
-    vid=videos[n-1]
-    destination=str(input("Enter your destination"))
-    vid.download(destination)
-    print(yt.filename+"\n Ha been downloaded")
-root=tk.Tk()
+from tkinter import *
+import pytube
 
-w=tk.Label(root,text="Youtube Downloader")
-w.pack()
+def download():
+    video_url = url.get()
+    try:
+        youtube = pytube.Youtube(video_url)
+        video   = youtube.streams.first()
+        video.download("C:/Users/Aethelwulf/Desktop/Videos")
+        notif.config(fg="green",text="Downloaded complete")
 
+    except Exception as e:
+        print(e)
+        notif.config(fg="red",text="Video could not be downloaded")
 
-E1=tk.Entry(root,bd=5)
-E1.pack(side=tk.TOP)
+master = Tk()
+master.title("Youtube video downloader")
+Label(master, text="Youtube Video Converter",fg="red",font=("Calibri",15)).grid(sticky=N,padx=100,row=0)
+Label(master, text="Enter the URL (Link) of your video below : ",fg="black",font=("Calibri",12)).grid(sticky=N,row=1,pady=15)
+notif = Label(master,font=("Celibri",12))
+notif.grid(sticky=N,pady=1,row=4)
+url = StringVar()
+Entry(master,width=50,textvariable=url).grid(sticky=N,row=2)
+Button(master,width=20,text="Download",font=("Celibri",12),command=download).grid(sticky=N,row=3,pady=15)
 
-
-button=tk.Button(root,text="Download",fg="red",command=downloadVid   )
-button.pack(side=tk.BOTTOM)
-
-root.mainloop()
+master.mainloop()
